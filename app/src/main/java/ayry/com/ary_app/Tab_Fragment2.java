a50e41e7b63cdbf65fb981ffe6e61ef07d02b47f
stephenkearns1 <Sean@Stephens-MacBook-Pro.local>
2016-02-19 09:23:44 +0000

    Initial commit- Project setup

---
+++ app/src/main/java/ayry/com/ary_app/Tab_Fragment2.java
@@ -0,0 +1,51 @@
+package ayry.com.ary_app;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Stephen J Kearns on 15/02/2016.
+ */
+
+public class Tab_Fragment2 extends Fragment {
+
+     /*
+          Code referance: http://guides.codepath.com/android/Google-Play-Style-Tabs-using-TabLayout
+     */
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //if arguments need to be returned
+        int someInt = getArguments().getInt("someInt",1);
+        String someTitle = getArguments().getString("someTitle","");
+
+        //what ever you want to do with arguments when returned
+    }
+
+    // new Instance constructor for creating fragment with arguments
+    public static Tab_Fragment2 newInstance(int page, String title){
+        //create new istance of the Tab_fragment1 class
+        Tab_Fragment2 tab_fragment2 = new Tab_Fragment2();
+        Bundle args = new Bundle();
+        args.putInt("someInt", 1);
+        args.putString("someTitle", title);
+        tab_fragment2.setArguments(args);
+
+        return tab_fragment2;
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment2_layout, container, false);
+
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/ayry/com/ary_app/Tab_fragment1.java
@@ -0,0 +1,49 @@
+package ayry.com.ary_app;
+
+/**
+ * Created by Stephen J Kearns on 14/02/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab_fragment1 extends Fragment{
+
+       /*
+          Code referance: http://guides.codepath.com/android/Google-Play-Style-Tabs-using-TabLayout
+     */
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //if arguments need to be returned
+        int someInt = getArguments().getInt("someInt",0);
+        String someTitle = getArguments().getString("someTitle","");
+
+        //what ever you want to do with arguments when returned
+    }
+
+    // new Instance constructor for creating fragment with arguments
+    public static Tab_fragment1 newInstance(int page, String title){
+        //create new istance of the Tab_fragment1 class
+        Tab_fragment1 tab_fragment1 = new Tab_fragment1();
+        Bundle args = new Bundle();
+        args.putInt("someInt", 0);
+        args.putString("someTitle", title);
+        tab_fragment1.setArguments(args);
+
+        return tab_fragment1;
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment1_layout, container, false);
+
+        return view;
+    }
+
+
+}
